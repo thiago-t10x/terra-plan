08bb2cd49958e37fec772d926f818b6e74952a2c
thiago-t10x <51928036+thiago-t10x@users.noreply.github.com>
2019-07-01 01:33:30 -0300

    Add files via upload

---
+++ Terra Plana/trocaimg.js
@@ -0,0 +1,11 @@
+function regua_horizonte (){
+  document.getElementById("trocarimg").src="imagens/regua_horizonte.jpg";
+}
+
+function sapato (){
+  document.getElementById("trocarimg").src="imagens/sapato.png";
+}
+
+function aviao (){
+  document.getElementById("trocarimg").src="imagens/aviao.jpg";
+}
